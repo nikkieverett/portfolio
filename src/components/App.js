ace082d3263a769dcc668279875a38a3057a62e4
Nikki Everett <neverett@modernize.com>
2020-07-26 00:35:01 -0500

    Waves layout solidified

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,105 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react'
 
 import Waves from './Waves'
 
 function App() {
+  const $app = useRef(null)
+
+  // useEffect(() => {
+  //   // Set up vars
+  //   const sections = document.querySelectorAll('.section')
+  //   const sectionsY = []
+
+  //   // Cache position of each panel
+  //   sections.forEach((el) => {
+  //     sectionsY.push(el.offsetTop)
+  //   })
+
+  //   // Update the window
+  //   function updateWindow() {
+  //     console.log(window)
+  //     var y = $app.scrollTop
+  //     var i;
+  //     var l;
+  //     console.log(y)
+
+  //     // Loop through our section positions
+  //     for (i = 0, l = sections.length; i < l; i++) {
+  //       /*
+  //           Firstly, we break if we're checking our last section,
+  //           otherwise we compare if he y position is in between
+  //           two panels
+  //       */
+
+  //       if (i === l - 1 || (y >= sectionsY[i] && y <= sectionsY[i + 1])) {
+  //         break
+  //       }
+  //     }
+
+  //     // Update classes
+  //     // sections.forEach((section) => {
+  //     //   section.classList.remove('section-fixed')
+  //     // })
+  //     // sections[i].classList.add('section-fixed')
+  //   }
+
+  //   // Bind our function to window scroll
+  //   $app.current.addEventListener('scroll', () => {
+  //     updateWindow()
+  //   })
+  // }, [])
+
   return (
-    <div className='app'>
-      <div className='header'>
-        <div className="header__container">
-          <div className='logo'></div>
-          <ul className='nav'>
-            <li className='nav__item'>
-              <button className='btn btn--secondary'>Projects</button>
-            </li>
-            <li className='nav__item'>
-              <button className='btn btn--secondary'>About Me</button>
-            </li>
-            <li className='nav__item'>
-              <button className='btn btn--secondary'>Skills</button>
-            </li>
-            <li className='nav__item'>
-              <button className='btn btn--secondary'>My Setup</button>
-            </li>
-            <li className='nav__item'>
-              <button className='btn btn--primary'>Contact Me</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='parallax-container'>
-        <div className="row">
-          <div className='col'>
-            <div className='content'>
-              <div className='content__title'>
-                <h1>
-                  Hello, world! <span className='wave-hello'></span>
-                </h1>
-                <h1>I'm Nikki.</h1>
+    <div className="app" ref={$app}>
+      <div className="header" />
+      <div className="section section-placeholder" />
+      <div className="section section-fixed">
+        <div className="section-inner">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm">
+                <div className="content">
+                  <div className="content__title">
+                    <h1>
+                      Hello, world! <span className="wave-hello" />
+                    </h1>
+                    <h1>I'm Nikki.</h1>
+                  </div>
+                  <div className="content__subtitle">
+                    <p>
+                      Front End Software Engineer
+                      <br />
+                      && UX Developer
+                    </p>
+                  </div>
+                  <div className="content__btn">
+                    <button className="btn btn--secondary" type="button">
+                      Contact Me
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className='content__subtitle'>
-                <p>
-                  Front End Software Engineer
-                  <br />
-                  && UX Developer
-                </p>
+              <div className="col-sm">
+                <div className="content">
+                  <div className="content__image" />
+                </div>
               </div>
-              <div className='content__btn'>
-                <button className='btn btn--secondary'>Contact Me</button>
-              </div>
-            </div>
-          </div>
-          <div className='col'>
-            <div className='content'>
-              <div className='content__image'></div>
             </div>
           </div>
         </div>
       </div>
-    <Waves />
+      <Waves />
+      <div className="section">
+        <h1>Section 2</h1>
+      </div>
+      <div className="section">
+        <h1>Section 3</h1>
+      </div>
+      <div className="section">
+        <h1>Section 4</h1>
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/components/Waves.js
+++ src/components/Waves.js
@@ -1,38 +1,44 @@
 import React, { useRef, useEffect } from 'react'
 import anime from 'animejs/lib/anime.es.js'
 
+import '../styles/waves.scss'
+
 export default function Waves() {
-	const wave1 = useRef(null)
-	const wave2 = useRef(null)
-	const wave3 = useRef(null)
-	const wave4 = useRef(null)
+  const wave1 = useRef(null)
+  const wave2 = useRef(null)
+  const wave3 = useRef(null)
+  const wave4 = useRef(null)
 
+  useEffect(() => {
+    const onScroll = () => {
+      anime({
+        targets: [wave1.current, wave2.current, wave3.current, wave4.current],
+        backgroundPositionX: anime.stagger([1000, 1400]),
+        loop: true,
+        keyframes: [{ backgroundPositionX: 0 }, { backgroundPositionX: 1400 }],
+        duration: (target, index) => {
+          if (index === 0 || index === 2) {
+            return 30000
+          }
 
-	useEffect(() => {
-		const topPosition = wave1.current.getBoundingClientRect().top
+          return 15000
+        },
+      })
+    }
 
-		const onScroll = () => {
-			const scrollPosition = window.scrollY + window.innerHeight
+    window.addEventListener('scroll', onScroll)
 
-			if (topPosition < scrollPosition) {
-				console.log('hi')
-				// trigger animation
-			}
-		}
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [])
 
-		window.addEventListener('scroll', onScroll)
-
-		return () => {
-			window.removeEventListener('scroll', onScroll)
-		}
-	}, [])
-
-	return (
+  return (
     <div className="waves">
-      <div className='wave wave--1' ref={wave1}></div>
-      <div className='wave wave--2' ref={wave2}></div>
-      <div className='wave wave--3' ref={wave3}></div>
-      <div className='wave wave--4' ref={wave4}></div>
+      <div className="wave wave--1" ref={wave1} />
+      <div className="wave wave--2" ref={wave2} />
+      <div className="wave wave--3" ref={wave3} />
+      <div className="wave wave--4" ref={wave4} />
     </div>
   )
 }
