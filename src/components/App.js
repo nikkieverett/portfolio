e08b1d800fae427a1d90fea470871e9598efb892
Nikki Everett <neverett@modernize.com>
2020-09-09 11:17:02 -0500

    Optimize images and code splitting for better performance

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
 
 import Waves from './Waves'
 import Header from './Header'
 import Hero from './sections/Hero'
-import About from './sections/About'
-import Projects from './sections/Projects'
-// import Skills from './sections/Skills'
-import Setup from './sections/Setup'
-import Contact from './sections/Contact'
+
+const About = lazy(() => import('./sections/About'))
+const Projects = lazy(() => import('./sections/Projects'))
+// const Skills = lazy(() => import('./sections/Skills'))
+const Setup = lazy(() => import('./sections/Setup'))
+const Contact = lazy(() => import('./sections/Contact'))
 
 function App() {
   const [waves, setWaves] = useState([])
 
   return (
     <div className="app">
-      <Header waves={waves} />
-      <div className="section section-placeholder" />
-      <Hero />
-      <Waves setWaves={setWaves} waves={waves} />
-      <Projects />
-      {/* <Skills /> */}
-      <Setup />
-      <About />
-      <Contact />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header waves={waves} />
+        <div className="section section-placeholder" />
+        <Hero />
+        <Waves setWaves={setWaves} waves={waves} />
+        <Projects />
+        {/* <Skills /> */}
+        <Setup />
+        <About />
+        <Contact />
+      </Suspense>
     </div>
   )
 }
