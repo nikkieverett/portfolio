cfc90f12fb45f3f700679ff35c3d139d7582ab35
Nikki Everett <neverett@modernize.com>
2020-09-17 12:48:35 -0500

    FB-3: popover wroking properly

--- src/components/Tooltip.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Tooltip({ parent, data }) {
-  return (
-    <div class="tooltip">
-      I'm a tooltip!
-    </div>
-  )
-}
--- src/data/setup.js
+++ src/data/setup.js
@@ -1,14 +1,48 @@
 const deskObjects = {
-  monitor: {},
-  keyboard: {},
-  mouse: {},
-  plant1: {},
-  plant2: {},
-  plant3: {},
-  coffee: {},
-  glasses: {},
-  laptop: {},
-  journal: {}
+  monitorRight: {
+    brand: 'Apple iMac',
+    features: ['27" Retina 5K Display', '10th Gen 6-core Intel® Core™ i5 Processor 3.1GHz', '802.11 Wireless-AC + Bluetooth 5.0']
+  },
+  monitorLeft: {
+    brand: 'Apple iMac',
+    features: ['27" Retina 5K Display', '10th Gen 6-core Intel® Core™ i5 Processor 3.1GHz', '802.11 Wireless-AC + Bluetooth 5.0']
+  },
+  keyBoard: {
+    brand: 'Apple Magic Keyboard',
+    features: ['Numeric Keypad', 'Space Grey']
+  },
+  mouse: {
+    brand: 'Logitech MX Master 3',
+    features: ['Meteorite Black', 'Wireless', 'Speed-Adaptive Scroll Wheel']
+  },
+  plant1: {
+    brand: 'Bunny Ear Cactus',
+    features: ['Nickname: Michelangelo', 'This cactus does not develop spines. Instead, it grows glochids, which are short whitish brown prickles.', 'Member of the prickly pear family']
+  },
+  plant2: {
+    brand: 'Moon Cactus',
+    features: ['Nickname: Raphael', "The top colorful portion of a moon cactus is technically a mutant that is not able to produce it's own chlorphyll. It has to be grafted on to the bottom cactus in order to survive", 'Member of the prickly pear family']
+  },
+  plant3: {
+    brand: 'Golden Pothos',
+    features: ['Nickname: Leonardo', 'The most indestructable plant known to man', "Seriously, I've forgotten to water this guy for several months"]
+  },
+  coffee: {
+    brand: 'Coffee',
+    features: ['Strong', "Beans: Peet's Nespresso Coffee Pods", "Milk: Moon Milk (if you've never heard of it, google Summermoon)"]
+  },
+  glasses: {
+    brand: 'Warby Parker Landon Frames',
+    features: ['Blue light blocking']
+  },
+  laptop: {
+    brand: 'MacBook Pro 15"',
+    features: ['2018', '2.9 GHz Intel Core i9', '32 GB 2400 MHz DDR4']
+  },
+  journal: {
+    brand: 'Best Self Journal',
+    features: ['13-week goal planner', 'Coral']
+  }
 }
 
 export default deskObjects
